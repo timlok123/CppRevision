e597bd5e3b9779d3286c74be6fec4cfbe25b3e8d
Tim Lok Chau <u3577808@connect.hku.hk>
2024-11-25 22:53:02 +0800

    Complete the function and derivative of Newton Method

--- Exercise/Newton_method.cpp
+++ Exercise/Newton_method.cpp
@@ -1,10 +1,26 @@
+/*  
+    This is a basic flow control example. 
+    The following example demonstrates the usage of 
+    if-else statment, for loop and while loop in C++. 
+*/
+
 #include <iostream>
 #include <cmath>
 
+double func(double x){
+    return std::exp(x) + std::pow(x,3) + 5;
+}
+
+double func_derivative(double x){
+    return std::exp(x) + 3*x*std::pow(x,2);
+}
+
+
 int main(){
 
-
-
+    double x = 2.0; 
+    cout<<func(x)<<endl;
+    cout<<func_derivative(x)<<endl;
 
     return 0; 
 }
